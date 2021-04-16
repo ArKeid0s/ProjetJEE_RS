df0376d6ea7214754cc21300edd2bc5a978bde3e
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2021-04-16 20:37:58 +0200

    New header with menu

--- src/com/proj/actions/CreatePost.java
+++ src/com/proj/actions/CreatePost.java
@@ -3,9 +3,6 @@ package com.proj.actions;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.servlet.http.HttpSession;
 
